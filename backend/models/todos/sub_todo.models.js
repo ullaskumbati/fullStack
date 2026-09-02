fcea1d9b8bd556763475b04f8a10651841803860
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-09-02 14:18:16 +0000

    todo model and subtodo models created

--- backend/models/todos/sub_todo.models.js
+++ backend/models/todos/sub_todo.models.js
@@ -1,22 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema(
-    {
-        username: {type :String,
-            required: true,
-            unique: true,
-            lowercase: true
-        },
-        email:{type: String,
-            required: true,
-            unique: true,
-            lowercase: true
-        },
-        password:{type: String,
-            required: true
-        },
-        isActive : Boolean,
+const subTodoSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    complete: {
+        type: Boolean,
+        default: false
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
     }
-);
+}, { timestamps: true });
 
-export const User = mongoose.model("User",userSchema);
+export const SubTodo = mongoose.model('SubTodo', subTodoSchema);
