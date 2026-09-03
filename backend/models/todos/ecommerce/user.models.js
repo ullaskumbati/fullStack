a9bd120e23d8a4e44e524a3ff9b2164e1d370430
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-09-03 14:17:03 +0000

    additional models added

---
+++ backend/models/todos/ecommerce/user.models.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+
+}, { timeStamps: true });
+
+export const User = mongoose.model("User", userSchema); 
