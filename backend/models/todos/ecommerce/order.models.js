f601d3434b739427f20ab0a1c939acd2db288cff
ullaskumbati <96108518+ullaskumbati@users.noreply.github.com>
2026-09-04 13:56:40 +0000

    models added

---
+++ backend/models/todos/ecommerce/order.models.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Prdouct"
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+
+const OrderSchema = new mongoose.Schema({
+    orderPrice: {
+        type: Number,
+        required: true,
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    orderItems: {
+        type: [orderItemSchema]
+    },
+    address:{
+        type: String,
+        required: true
+    },
+    status:{
+        type: String,
+        enum: ["PENDING","CANCELLED","DELIVERED"],
+        default: "PENDING"
+    }
+}, { timestamps: true });
+
+export const Order = mongoose.model("Order", OrderSchema);
